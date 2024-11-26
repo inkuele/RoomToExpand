01b258be99c5e103692f45a49f0afe5f53f8caf1
Marcello <Marcello.lussana@student.hu-berlin.de>
2024-11-26 14:13:03 +0100

    fix batch file path to *.dir directories

--- GPT_Doc_Files/Scripts/batch_concat.sh
+++ GPT_Doc_Files/Scripts/batch_concat.sh
@@ -5,6 +5,6 @@
 cd "$(dirname "$0")"
 
 # Find all .dir directories in the parent directory and pass each to the Python script
-for filename in ../*.dir; do
+for filename in ../TD_Patch_Files/*.dir; do
     python3 concat_dir_files.py "$filename"
 done
